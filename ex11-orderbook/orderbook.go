17a7d14a767658cc1147270ae90a2a4428bad6dc
Andrew Peresada <kohelbekker@gmail.com>
2019-08-22 18:40:32 +0300

    WIP ex11

--- ex11-orderbook/orderbook.go
+++ ex11-orderbook/orderbook.go
@@ -1,15 +1,52 @@
 package orderbook
 
 type Orderbook struct {
-	// TODO
+	Bids   []*Order
+	Asks   []*Order
+	Trades []*Trade
+	///
 }
 
 func New() *Orderbook {
-	// TODO
-	return &Orderbook{}
+	var OrdBook Orderbook = &Orderbook{}
+	OrdBook.Bids = &*Order{}
+	OrdBook.Asks = &*Order{}
+	OrdBook.Trade = &*Trade{}
+	return OrdBook
 }
 
-func (orderbook *Orderbook) Match(order *Order) ([]*Trade, *Order) {
-	// TODO
+func BidMarketOrder(OrdBook *Orderbook, order *Order) ([]*Trade, *Order) {
+
+}
+
+func BidLimitOrder(OrdBook *Orderbook, order *Order) ([]*Trade, *Order) {
+
+}
+
+func BidLimitOrder(OrdBook *Orderbook, order *Order) ([]*Trade, *Order) {
+
+}
+
+func BidLimitOrder(OrdBook *Orderbook, order *Order) ([]*Trade, *Order) {
+
+}
+
+func (OrdBook *Orderbook) Match(order *Order) ([]*Trade, *Order) {
+	switch Order.Side {
+	case SideBid:
+		switch Order.Kind {
+		case KindMarket:
+			BidMarketOrder(OrdBook, order)
+		case KindLimit:
+			BidLimitOrder(OrdBook, order)
+		}
+	case SideAsk:
+		switch Order.Kind {
+		case KindMarket:
+			AskMarketOrder(OrdBook, order)
+		case KindLimit:
+			AskLimitOrder(OrdBook, order)
+		}
+	}
 	return nil, nil
 }
